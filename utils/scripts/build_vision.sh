c732608be1046eea7c3720568227acacd9024c70
R-ZWEI-Kickers <rzwei.kickers@hs-kl.de>
2026-09-17 12:32:55 +0200

    Delete uneeden ros2 msg definitions from utils folder, copy missing files to core ros2_ws folder and change build ref to new location

--- utils/scripts/build_vision.sh
+++ utils/scripts/build_vision.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Clean rebuild of vision + interface packages for the remote robot.
+# Msg packages (booster_msgs, booster_ros2_interface, vision_interface) live
+# in core/src/ros2_ws/src/. This script symlinks them into utils/ so colcon
+# can build them alongside utils/vision/ in one workspace.
+#
 # Usage: ./build_vision.sh [extra colcon args...]
 set -e
 
@@ -7,16 +11,21 @@ cd "$(dirname "$0")/.."
 
 source /opt/ros/humble/setup.bash
 
-echo "[1/3] Removing build/, install/, log/"
+echo "[1/4] Symlinking ros2_ws msg packages into workspace"
+ln -sf ../core/src/ros2_ws/src/booster_msgs booster_msgs
+ln -sf ../core/src/ros2_ws/src/booster_ros2_interface booster_ros2_interface
+ln -sf ../core/src/ros2_ws/src/vision_interface vision_interface
+
+echo "[2/4] Removing build/, install/, log/"
 rm -rf build install log
 
-echo "[2/3] colcon build (vision_interface, booster_interface, booster_msgs, vision)"
+echo "[3/4] colcon build (vision_interface, booster_interface, booster_msgs, vision)"
 colcon build --symlink-install \
   --packages-select vision_interface booster_interface booster_msgs vision \
   --cmake-args -Wno-dev \
   "$@"
 
-echo "[3/3] Sourcing overlay"
+echo "[4/4] Sourcing overlay"
 source install/setup.bash
 
 echo ""
